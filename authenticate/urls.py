c7b591a3f6ab712224c195b4c58c8401105a8835
Uvaismon <uvaisullas@gmail.com>
2021-09-27 18:16:47 +0530

    Logout link bug fixed

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -5,7 +5,7 @@ from .views import (RegisterStudent, Login, Logout, RegisterUniversity)
 urlpatterns = [
     path('register/student', RegisterStudent.as_view(), name='register_student'),
     path('login/<str:user>', Login.as_view(), name='login_student'),
-    path('logout/<str:user>', Logout.as_view(), name='logout_student'),
+    path('logout', Logout.as_view(), name='logout_student'),
     path('register/university', RegisterUniversity.as_view(),
          name='register_university'),
         
