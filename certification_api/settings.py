5b8317f269ea0bdffe0fca2fa1903ab67fc41611
Uvaismon <uvaisullas@gmail.com>
2021-11-03 12:43:17 +0530

    Added NEU databased creds

--- certification_api/settings.py
+++ certification_api/settings.py
@@ -91,10 +91,24 @@ WSGI_APPLICATION = 'certification_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'advisor',
+        'USER': 'app',
+        'PASSWORD': 'Hd9msAwqPyd',
+        'HOST': 'app.neu.edu.tr',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
